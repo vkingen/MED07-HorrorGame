8d63c2233e23fa2438b7a8aa916bf6db45b9e845
vkingen <lassevikingkristensen@gmail.com>
2024-11-21 15:31:15 +0100

    Wall Writing Audio

--- Assets/Group Members/Lasse/Textures/AnimateBloodWriting.cs
+++ Assets/Group Members/Lasse/Textures/AnimateBloodWriting.cs
@@ -4,12 +4,14 @@ public class AnimateBloodWriting : MonoBehaviour
 {
     public Material writingMaterial; // Assign the material here
     public float revealSpeed = 0.5f; // Speed of the reveal animation
+    AudioSource audioSource;
 
     private float revealProgress = 1;
     private bool isStarted = false;
     public bool isDone = false;
     private void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         revealProgress = 1;
         writingMaterial.SetFloat("_RevealProgress", revealProgress);
     }
@@ -17,6 +19,7 @@ public class AnimateBloodWriting : MonoBehaviour
     public void Write()
     {
         isStarted = true;
+        audioSource.Play();
     }
 
     void Update()
